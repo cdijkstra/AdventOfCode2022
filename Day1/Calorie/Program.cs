3b16b8e67cf95b7df5d1a0fa2951e8e6e8ea7ef1
casper <cdijkstra@xpirit.com>
2022-12-01 19:18:46 +0100

    Refactor to foreach loop

--- Day1/Calorie/Program.cs
+++ Day1/Calorie/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("testinput.txt");
+var lineEntries = lines.Length;
+
+List<int> listOfCounts = new();
+var count = 0;
+foreach (var calory in lines)
+{
+    if (!String.IsNullOrEmpty(calory))
+    {
+        count += int.Parse(calory);
+    }
+    else
+    {
+        listOfCounts.Add(count);
+        count = 0;
+    }
+}
+listOfCounts.Add(count);
+
+var answer = listOfCounts.OrderByDescending(x => x).Take(3).Sum();
+Console.WriteLine(answer);
