a3afdd7363eecf5541b91eb0898ffbb4981a0080
casper <cdijkstra@xpirit.com>
2022-12-03 13:24:17 +0100

    Solution for Rucksack

---
+++ Day2/RockPaperScissors/Program.cs
@@ -0,0 +1,102 @@
+﻿string[] games = File.ReadAllLines("input.txt");
+int totalScore = 0;
+
+foreach (var game in games)
+{
+    var otherHand = char.Parse(game.Split( )[0]);
+    var yourHand = char.Parse(game.Split( )[1]);
+    switch (otherHand)
+    {
+        case 'A':
+            totalScore += playGame2(otherHand, yourHand);
+            break;
+        case 'B':
+            totalScore += playGame2(otherHand, yourHand);
+            break;
+        case 'C':
+            totalScore += playGame2(otherHand, yourHand);
+            break;
+    }
+}
+
+Console.WriteLine(totalScore);
+
+int playGame1(char otherHand, char yourHand)
+{
+    Dictionary<char, int> pointsBonus = new()
+    {
+        {'X', 1},
+        {'Y', 2},
+        {'Z', 3}
+    };
+
+    Dictionary<char, char> convertHand = new()
+    {
+        {'A', 'X'},
+        {'B', 'Y'},
+        {'C', 'Z'}
+    };
+    
+    var points = 0;
+    if (convertHand[otherHand] == yourHand)
+    {
+        points += 3;
+    }
+    else if ((char) (convertHand[otherHand] + 1) == yourHand || (char) (convertHand[otherHand] - 2) == yourHand) 
+    {
+        points += 6;
+    }
+
+    points += pointsBonus[yourHand];
+    return points;
+}
+
+int playGame2(char otherHand, char yourHand)
+{
+    Dictionary<char, int> pointsBonus = new()
+    {
+        {'X', 1},
+        {'Y', 2},
+        {'Z', 3}
+    };
+
+        Dictionary<char, char> findSameCard = new()
+    {
+        {'A', 'X'},
+        {'B', 'Y'},
+        {'C', 'Z'}
+    };
+
+    Dictionary<char, char> findLosingCard = new()
+    {
+        {'A', 'Z'},
+        {'B', 'X'},
+        {'C', 'Y'}
+    };
+
+    Dictionary<char, char> findWinningCard = new()
+    {
+        {'A', 'Y'},
+        {'B', 'Z'},
+        {'C', 'X'}
+    };
+
+    var points = 0;
+    if (yourHand == 'X')
+    {
+        // We have to lose
+        points += pointsBonus[findLosingCard[otherHand]];
+    }
+    else if (yourHand == 'Y') 
+    {
+        // We have to play a draw
+        points += 3 + pointsBonus[findSameCard[otherHand]];
+    }
+    else 
+    {
+        // We have to win
+        points += 6 + pointsBonus[findWinningCard[otherHand]];
+
+    }
+    return points;
+}
---
+++ Day3/Rucksack/Program.cs
@@ -0,0 +1,76 @@
+﻿string[] rucksackContent = File.ReadAllLines("data.txt");
+
+List<int> listOfCounts = new();
+var matchedList = new List<char>();
+
+var groupList = new List<char>();
+var secondGroupList = new List<char>();
+var thirdGroupList = new List<char>();
+
+var uniqueForGroupList = new List<char>();
+
+var moduloThree = 0;
+
+foreach (var content in rucksackContent)
+{
+    var firstCharList = new List<char>();
+    var secondCharList = new List<char>();
+    
+    var contentLength = content.Length;
+    for (int idx = 0; idx < contentLength; idx++)
+    {
+        if (idx < contentLength / 2)
+        {
+            firstCharList.Add(content[idx]);
+        }
+        else
+        {
+            secondCharList.Add(content[idx]);
+        }
+        
+        if (moduloThree % 3 == 0)
+        {
+            groupList.Add(content[idx]);
+        }
+        else if (moduloThree % 3 == 1)
+        {
+            secondGroupList.Add(content[idx]);
+        }
+        else
+        {
+            thirdGroupList.Add(content[idx]);
+        }
+    }
+
+    var similarEntries = firstCharList.Where(c => secondCharList.Contains(c)).Distinct().ToList();
+    matchedList.AddRange(similarEntries);
+    
+    if (moduloThree % 3 == 2)
+    {
+        // Find unique entry
+        var similarEntry = groupList.Where(c => secondGroupList.Contains(c) && thirdGroupList.Contains(c))
+            .Distinct().First();
+
+        uniqueForGroupList.Add(similarEntry);
+        groupList = new();
+        secondGroupList = new();
+        thirdGroupList = new();
+    }
+    
+    moduloThree++;
+}
+
+var totalScore = 0;
+var totalGroupScore = 0;
+
+foreach (var match in matchedList)
+{
+    totalScore += char.IsUpper(match) ? match - 38 : match - 96;
+}
+
+foreach (var match in uniqueForGroupList)
+{
+    totalGroupScore += char.IsUpper(match) ? match - 38 : match - 96;
+}
+
+Console.WriteLine($"Result for part 1 = {totalScore} and result for part 2 = {totalGroupScore}");
