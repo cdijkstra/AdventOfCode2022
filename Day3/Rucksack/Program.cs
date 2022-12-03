bd69fb750570276b8276fdeb90357ac044827d2b
casper <cdijkstra@xpirit.com>
2022-12-03 23:11:59 +0100

    Beautify code

--- Day3/Rucksack/Program.cs
+++ Day3/Rucksack/Program.cs
@@ -1,76 +1,51 @@
-﻿string[] rucksackContent = File.ReadAllLines("data.txt");
+﻿string[] fullRucksackContent = File.ReadAllLines("data.txt");
 
-List<int> listOfCounts = new();
 var matchedList = new List<char>();
 
-var groupList = new List<char>();
-var secondGroupList = new List<char>();
-var thirdGroupList = new List<char>();
-
+var rucksackContent = new List<char>();
+var contentOfRucksackGroup = new List<List<char>>(); // Filled with three rucksackContents; then emptied again
 var uniqueForGroupList = new List<char>();
 
-var moduloThree = 0;
-
-foreach (var content in rucksackContent)
+for (var rucksackIndex = 0; rucksackIndex < fullRucksackContent.Length; rucksackIndex++)
 {
+    var content = fullRucksackContent[rucksackIndex];
+    rucksackContent.AddRange(content);
+    var contentLength = content.Length;
+
     var firstCharList = new List<char>();
     var secondCharList = new List<char>();
-    
-    var contentLength = content.Length;
-    for (int idx = 0; idx < contentLength; idx++)
-    {
-        if (idx < contentLength / 2)
-        {
-            firstCharList.Add(content[idx]);
-        }
-        else
-        {
-            secondCharList.Add(content[idx]);
-        }
-        
-        if (moduloThree % 3 == 0)
-        {
-            groupList.Add(content[idx]);
-        }
-        else if (moduloThree % 3 == 1)
-        {
-            secondGroupList.Add(content[idx]);
-        }
-        else
-        {
-            thirdGroupList.Add(content[idx]);
-        }
-    }
+    firstCharList.AddRange(content.Substring(0, contentLength / 2).ToCharArray().ToList());
+    secondCharList.AddRange(content.Substring(contentLength / 2, contentLength / 2).ToCharArray().ToList());
 
     var similarEntries = firstCharList.Where(c => secondCharList.Contains(c)).Distinct().ToList();
     matchedList.AddRange(similarEntries);
     
-    if (moduloThree % 3 == 2)
+    contentOfRucksackGroup.Add(rucksackContent);
+    
+    if (rucksackIndex % 3 == 2)
     {
         // Find unique entry
-        var similarEntry = groupList.Where(c => secondGroupList.Contains(c) && thirdGroupList.Contains(c))
-            .Distinct().First();
+        var similarEntry = contentOfRucksackGroup[0]
+            .Where(c => 
+                contentOfRucksackGroup[1].Contains(c) && 
+                contentOfRucksackGroup[2].Contains(c)
+                )
+            .Distinct()
+            .First();
 
         uniqueForGroupList.Add(similarEntry);
-        groupList = new();
-        secondGroupList = new();
-        thirdGroupList = new();
+        contentOfRucksackGroup.Clear();
     }
-    
-    moduloThree++;
+
+    rucksackContent.Clear(); // Reuse rucksackContent
 }
 
-var totalScore = 0;
-var totalGroupScore = 0;
-
-foreach (var match in matchedList)
-{
-    totalScore += char.IsUpper(match) ? match - 38 : match - 96;
-}
-
-foreach (var match in uniqueForGroupList)
-{
-    totalGroupScore += char.IsUpper(match) ? match - 38 : match - 96;
-}
+var totalScore = matchedList.Sum(match => AddScoreForChar(match));
+var totalGroupScore = uniqueForGroupList.Sum(match => AddScoreForChar(match));
 
 Console.WriteLine($"Result for part 1 = {totalScore} and result for part 2 = {totalGroupScore}");
+
+int AddScoreForChar(char match1)
+{
+    return char.IsUpper(match1) ? match1 - 38 : match1 - 96;
+}
