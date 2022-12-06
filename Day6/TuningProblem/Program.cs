43b9787a95b11f707b06279770fb3fb98cf1bceb
casper <cdijkstra@xpirit.com>
2022-12-06 09:58:43 +0100

    Use var everywhere

--- Day6/TuningProblem/Program.cs
+++ Day6/TuningProblem/Program.cs
@@ -10,7 +10,7 @@ public class Tuning
         var signal = File.ReadAllText(file);
         for (var idx = charLength; idx < signal.Length; idx++)
         {
-            List<char> previousEntries = signal.Substring(idx - charLength, charLength).ToList();
+            var previousEntries = signal.Substring(idx - charLength, charLength).ToList();
             if (previousEntries.Distinct().Count() == charLength)
             {
                 return idx;
@@ -21,15 +21,14 @@ public class Tuning
     }
 }
 
-class Program
+internal static class Program
 {
     static void Main(string[] args)
     {
         var tuning = new Tuning();
         tuning.SolveProblems("dummydata.txt", false).Should().Be(7);
         tuning.SolveProblems("dummydata.txt", true).Should().Be(19);
-        // tuning.FindPartiallyOverlappingSections("dummydata.txt").Should().Be(4);
-
+        
         var firstResult = tuning.SolveProblems("data.txt", false);
         var secondResult = tuning.SolveProblems("data.txt", true);
         Console.WriteLine($"{firstResult} and {secondResult}");
