051e1f546b78c44cc77f74d171df30a0f6e8f16f
casper <cdijkstra@xpirit.com>
2022-12-06 09:57:16 +0100

    Tuning problem

--- Day6/TuningProblem/Program.cs
+++ Day6/TuningProblem/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FluentAssertions;
+
+namespace Tuning;
+
+public class Tuning
+{
+    public int SolveProblems(string file, bool secondExercise)
+    {
+        var charLength = secondExercise ? 14 : 4;
+        var signal = File.ReadAllText(file);
+        for (var idx = charLength; idx < signal.Length; idx++)
+        {
+            List<char> previousEntries = signal.Substring(idx - charLength, charLength).ToList();
+            if (previousEntries.Distinct().Count() == charLength)
+            {
+                return idx;
+            }
+        }
+
+        return 0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var tuning = new Tuning();
+        tuning.SolveProblems("dummydata.txt", false).Should().Be(7);
+        tuning.SolveProblems("dummydata.txt", true).Should().Be(19);
+        // tuning.FindPartiallyOverlappingSections("dummydata.txt").Should().Be(4);
+
+        var firstResult = tuning.SolveProblems("data.txt", false);
+        var secondResult = tuning.SolveProblems("data.txt", true);
+        Console.WriteLine($"{firstResult} and {secondResult}");
+    }
+}
