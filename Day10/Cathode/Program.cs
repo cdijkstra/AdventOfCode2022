f0716a40c2e05a4ce0afe92f64b5347832f357ba
casper <cdijkstra@xpirit.com>
2022-12-10 14:11:45 +0100

    Solution for part 2

--- Day10/Cathode/Program.cs
+++ Day10/Cathode/Program.cs
@@ -1,10 +1,13 @@
-﻿using FluentAssertions;
+﻿using System.Reflection.Metadata;
+using FluentAssertions;
 
 namespace Tube;
 
 class Tube
 {
     private List<(int cycle, int signal)> _signals = new();
+    private List<char> _image = new();
+    
     public int Solution1(string file)
     {
         var currentSignal = 1;
@@ -23,9 +26,6 @@ class Tube
                 var addSignal = int.Parse(instruction.Split()[1]);
                 (int cycle, int signal) secondEntry = (++currentCycle, currentSignal += addSignal);
                 _signals.Add(secondEntry);
-                
-                var addSecondPixel = Math.Abs(currentCycle - currentSignal) <= 1 ? '#' : '.';
-                Console.WriteLine($"Adding {addSignal} ({addSecondPixel}) to {currentSignal} at {currentCycle}");
             }
 
             currentCycle++;
@@ -40,6 +40,41 @@ class Tube
 
         return answer;
     }
+    
+    public List<char> Solution2(string file)
+    {
+        var currentSignal = 1;
+        var currentCycle = 1;
+        _image = new();
+        
+        _signals.Clear();
+        _signals.Add((0, 0)); // So we can use index 1 instead of 0 ;)
+
+        var instructions = File.ReadLines(file);
+        foreach (var instruction in instructions)
+        {
+            (int cycle, int signal) firstEntry = (currentCycle, currentSignal);
+            _signals.Add(firstEntry);
+
+            var addCharToImage = Math.Abs(currentCycle % 40 - currentSignal - 1) <= 1 ? '#' : '.';
+            _image.Add(addCharToImage);
+
+            if (instruction.StartsWith("addx"))
+            {
+                ++currentCycle;
+                var addToImage = Math.Abs(currentCycle % 40 - currentSignal - 1) <= 1 ? '#' : '.';
+                var addSignal = int.Parse(instruction.Split()[1]);
+                _image.Add(addToImage);
+                
+                (int cycle, int signal) secondEntry = (currentCycle, currentSignal += addSignal);
+                _signals.Add(secondEntry);
+            }
+
+            currentCycle++;
+        }
+
+        return _image;
+    }
 }
 
 class Program
@@ -48,10 +83,19 @@ class Program
     {
         var tube = new Tube();
         tube.Solution1("dummydata.txt").Should().Be(13140);
-        // tube.Solution2("dummydata.txt").Should().Be("MCD");
-        //
-        // var firstResult = tube.Solution1("data.txt");
-        // var secondResult = supply.Solution2("data.txt");
-        // Console.WriteLine($"Results are {firstResult}");
+        var expectedResult = "##..##..##..##..##..##..##..##..##..##..###...###...###...###...###...###...###.####....####....####....####....####....#####.....#####.....#####.....#####.....######......######......######......###.#######.......#######.......#######.....".ToCharArray().ToList();
+        tube.Solution2("dummydata.txt").Should().BeEquivalentTo(expectedResult);
+        
+        var answer1 = tube.Solution1("data.txt");
+        var answer2 = tube.Solution2("data.txt");
+        Console.WriteLine($"First answer = {answer1} and the second answer is below;");
+        for (var idx = 0; idx != answer2.Count; idx++)
+        {
+            Console.Write(answer2[idx]);
+            if (idx >= 30 && (idx + 1) % 40 == 0)
+            {
+                Console.WriteLine("");
+            }
+        }
     }
 }
