b50ac2f91e7b0c892c55be4d468684e02a970e7b
casper <cdijkstra@xpirit.com>
2022-12-10 13:26:30 +0100

    Solution Day 10

---
+++ Day10/Cathode/Program.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+
+namespace Tube;
+
+class Tube
+{
+    private List<(int cycle, int signal)> _signals = new();
+    public int Solution1(string file)
+    {
+        var currentSignal = 1;
+        var currentCycle = 1;
+        _signals.Clear();
+        _signals.Add((0, 0)); // So we can use index 1 instead of 0 ;)
+
+        var instructions = File.ReadLines(file);
+        foreach (var instruction in instructions)
+        {
+            (int cycle, int signal) firstEntry = (currentCycle, currentSignal);
+            _signals.Add(firstEntry);
+
+            if (instruction.StartsWith("addx"))
+            {
+                var addSignal = int.Parse(instruction.Split()[1]);
+                (int cycle, int signal) secondEntry = (++currentCycle, currentSignal += addSignal);
+                _signals.Add(secondEntry);
+                
+                var addSecondPixel = Math.Abs(currentCycle - currentSignal) <= 1 ? '#' : '.';
+                Console.WriteLine($"Adding {addSignal} ({addSecondPixel}) to {currentSignal} at {currentCycle}");
+            }
+
+            currentCycle++;
+        }
+
+        var answer = 0;
+        for (var idx = 20; idx <= 240; idx += 40)
+        {
+            var newEntry = (_signals[idx - 1].signal * _signals[idx].cycle);
+            answer += newEntry;
+        }
+
+        return answer;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var tube = new Tube();
+        tube.Solution1("dummydata.txt").Should().Be(13140);
+        // tube.Solution2("dummydata.txt").Should().Be("MCD");
+        //
+        // var firstResult = tube.Solution1("data.txt");
+        // var secondResult = supply.Solution2("data.txt");
+        // Console.WriteLine($"Results are {firstResult}");
+    }
+}
--- Day9/RopeBridge/Program.cs
+++ Day9/RopeBridge/Program.cs
@@ -14,9 +14,8 @@ public enum Direction
 public class RopeBridge
 {
     private List<List<char>> _grid = new ();
-    private (int x, int y) _positionHead = new();
-    private (int x, int y) _postitionTail = new();
-    
+    private List<(int x, int y)> _knots = new(); // Exercise 2
+
     private const int _xMax = 5000;
     private const int _yMax = 5000;
     public int SolveProblem1(string file)
@@ -59,11 +58,6 @@ public class RopeBridge
                     break;
                 }
             }
-            // foreach (var entry in _grid)
-            // {
-            //     entry.ForEach(x => Console.Write(x));
-            //     Console.WriteLine();
-            // }
         }
 
         return _grid.SelectMany(x => x).Count(y => y.Equals('#'));
@@ -72,115 +66,163 @@ public class RopeBridge
     private void MoveRight(int amount)
     {
         int moveTailRightBy = CalculateStepsRight(amount);
-        
+
+        var newHeadX = _knots[0].x + amount;
+        var newTailX = _knots[1].x + moveTailRightBy;
+        var newTailY = _knots[1].y;
         if (moveTailRightBy > 0)
         {
-            switch (_positionHead.y - _postitionTail.y)
+            switch (_knots[0].y - _knots[1].y)
             {
                 case 1:
-                    _postitionTail.y++;
+                    newTailY++;
                     break;
                 case -1:
-                    _postitionTail.y--;
+                    newTailY--;
                     break;
             }
 
             for (var moveRight = 1; moveRight <= moveTailRightBy; moveRight++)
             {
-                _grid[_postitionTail.x + moveRight][_postitionTail.y] = '#';
+                _grid[_knots[1].x + moveRight][newTailY] = '#';
             }
         }
 
-        _postitionTail.x += moveTailRightBy;
-        _positionHead.x += amount;
+        List<(int x, int y)> newKnots = new()
+        {
+            (newHeadX, _knots[0].y),
+            (newTailX, newTailY),
+        };
+
+        _knots = newKnots;
     }
 
     private void MoveLeft(int amount)
     {
         int moveTailLeftBy = CalculateStepsLeft(amount);
+        var newTailY = _knots[1].y;
         if (moveTailLeftBy > 0)
         {
-            switch (_positionHead.y - _postitionTail.y)
+            switch (_knots[0].y - _knots[1].y)
             {
                 case 1:
-                    _postitionTail.y++;
+                    newTailY++;
                     break;
                 case -1:
-                    _postitionTail.y--;
+                    newTailY--;
                     break;
             }
 
             for (var moveLeft = 1; moveLeft <= moveTailLeftBy; moveLeft++)
             {
-                _grid[_postitionTail.x - moveLeft][_postitionTail.y] = '#';
+                _grid[_knots[1].x - moveLeft][newTailY] = '#';
             }
         }
+        
+        var newTailX = _knots[1].x - moveTailLeftBy;
+        var newHeadX = _knots[0].x - amount;
 
-        _postitionTail.x -= moveTailLeftBy;
-        _positionHead.x -= amount;
+        List<(int x, int y)> newKnots = new()
+        {
+            (newHeadX, _knots[0].y),
+            (newTailX, newTailY),
+        };
+
+        _knots = newKnots;
+        
     }
 
     private void MoveDown(int amount)
     {
         int moveTailDownBy = CalculateStepsDown(amount);
+        var newTailX = _knots[1].x;
         if (moveTailDownBy > 0)
         {
-            switch (_positionHead.x - _postitionTail.x)
+            switch (_knots[0].x - _knots[1].x)
             {
                 case 1:
-                    _postitionTail.x++;
+                    newTailX++;
                     break;
                 case -1:
-                    _postitionTail.x--;
+                    newTailX--;
                     break;
             }
 
             for (var moveDown = 1; moveDown <= moveTailDownBy; moveDown++)
             {
-                _grid[_postitionTail.x][_postitionTail.y - moveDown] = '#';
+                _grid[newTailX][_knots[1].y - moveDown] = '#';
             }
         }
 
-        _postitionTail.y -= moveTailDownBy;
-        _positionHead.y -= amount;
+        var newTailY = _knots[1].y - moveTailDownBy;
+        var newHeadY = _knots[0].y - amount;
+        
+        List<(int x, int y)> newKnots = new()
+        {
+            (_knots[0].x, newHeadY),
+            (newTailX, newTailY),
+        };
     }
 
     private void MoveUp(int amount)
     {
         int moveTailUpBy = CalculateStepsUp(amount);
+        var newTailX = _knots[1].x;
         if (moveTailUpBy > 0)
         {
-            switch (_positionHead.x - _postitionTail.x)
+            switch (_knots[0].x - _knots[1].x)
             {
                 case 1:
-                    _postitionTail.x++;
+                    newTailX++;
                     break;
                 case -1:
-                    _postitionTail.x--;
+                    newTailX--;
                     break;
             }
 
             for (var moveUp = 1; moveUp <= moveTailUpBy; moveUp++)
             {
-                _grid[_postitionTail.x][_postitionTail.y + moveUp] = '#';
+                _grid[newTailX][_knots[1].y + moveUp] = '#';
             }
         }
 
-        _postitionTail.y += moveTailUpBy;
-        _positionHead.y += amount;
+        var newTailY = _knots[1].y + moveTailUpBy;
+        var newHeadY = _knots[0].y + amount;
+        
+        List<(int x, int y)> newKnots = new()
+        {
+            (_knots[0].x, newHeadY),
+            (newTailX, newTailY),
+        };
     }
 
     public int SolveProblem2(string file)
     {
         Initialize(file);
-        
-        return 0;
+
+        return 1;
+    }
+
+    private int findElementOfFirstDuplicate()
+    {
+        for (var listIndex = 0; listIndex < _knots.Count - 1; listIndex++)
+        {
+            if (_knots[listIndex] == _knots[listIndex + 1])
+                return listIndex;
+        }
+
+        return 10; // No duplicates
     }
 
     private void Initialize(string file)
     {
-        _positionHead = (_xMax / 2 - 1, _yMax / 2 - 1);
-        _postitionTail = (_xMax / 2 - 1, _yMax / 2 - 1);
+        _knots.Clear();
+        var currentXPos = _xMax / 2 - 1;
+        var currentYPos = _yMax / 2 - 1;
+        var head = (currentXPos, currentYPos);
+        var tail = (currentXPos, currentYPos);
+        _knots.Add(head);
+        _knots.Add(tail);
         _grid.Clear();
 
         for (var idx = 0; idx < _xMax; idx++)
@@ -188,12 +230,15 @@ public class RopeBridge
             List<char> row = Enumerable.Repeat('.', _yMax).ToList();
             _grid.Add(row);
         }
-        _grid[_positionHead.x][_positionHead.y] = '#';
+        
+        _knots = Enumerable.Repeat( ( currentXPos, currentYPos ), 10).ToList();
+        
+        _grid[_knots[0].x][_knots[0].y] = '#';
     }
     
     private int CalculateStepsUp(int amount)
     {
-        return (_positionHead.y - _postitionTail.y) switch
+        return (_knots[0].y - _knots[0].y) switch
         {
             1 => Math.Max(amount, 0),
             0 => Math.Max(amount - 1, 0),
@@ -204,7 +249,7 @@ public class RopeBridge
     
     private int CalculateStepsRight(int amount)
     {
-        return (_positionHead.x - _postitionTail.x) switch
+        return (_knots[0].x - _knots[1].x) switch
         {
             1 => Math.Max(amount, 0),
             0 => Math.Max(amount - 1, 0),
@@ -215,7 +260,7 @@ public class RopeBridge
     
     private int CalculateStepsLeft(int amount)
     {
-        return (_positionHead.x - _postitionTail.x) switch
+        return (_knots[0].x - _knots[1].x) switch
         {
             1 => Math.Max(amount - 2, 0),
             0 => Math.Max(amount - 1, 0),
@@ -226,7 +271,7 @@ public class RopeBridge
     
     private int CalculateStepsDown(int amount)
     {
-        return (_positionHead.y - _postitionTail.y) switch
+        return (_knots[0].y - _knots[1].y) switch
         {
             1 => Math.Max(amount - 2, 0),
             0 => Math.Max(amount - 1, 0),
@@ -236,7 +281,6 @@ public class RopeBridge
     }
 }
 
-
 internal static class Program
 {
     static void Main(string[] args)
@@ -245,9 +289,9 @@ internal static class Program
         ropeBridge.SolveProblem1("dummydata.txt").Should().Be(13);
         // treetop.SolveProblem2("dummydata.txt").Should().Be(8);
         //
-        var solution1 = ropeBridge.SolveProblem1("data.txt");
+        // var solution1 = ropeBridge.SolveProblem1("data.txt");
         // var solution2 = treetop.SolveProblem2("data.txt");
         //
-        Console.WriteLine($"Solutions are {solution1}");
+        // Console.WriteLine($"Solutions are {solution1}");
     }
 }
