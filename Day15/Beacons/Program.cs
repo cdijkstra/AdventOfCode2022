60af0464620849ef5c0a050876737ab682cf97fb
casper <cdijkstra@xpirit.com>
2022-12-19 20:04:57 +0100

    Ugly solution

--- Day15/Beacons/Program.cs
+++ Day15/Beacons/Program.cs
@@ -6,14 +6,7 @@ public class Beacons
 {
     private List<(int x, int y, int xBeacon, int yBeacon, int distanceBeacon)> _data = new();
     private List<(int x, int y)> _excludedPointsRow = new();
-
-    // private int countPointsInManhattanCircle(int sphere)
-    // {
-    //     if (sphere == 0)
-    //         return 1;
-    //     
-    //     return 4 * sphere + countPointsInManhattanCircle(sphere - 1);
-    // }
+    private List<List<bool>> _beacons = new();
     
     private void FillRow(int row)
     {
@@ -45,61 +38,7 @@ public class Beacons
     {
         Initialize(file);
         FillRow(row);
-        
-        // Calculate amount of overlapping points in spheres.
-        // Add all points and subtract all duplicates!
-        // int excludedPlaces = 0;
-        // foreach (var valueTuple in _data)
-        // {
-        //     excludedPlaces += countPointsInManhattanCircle(valueTuple.distanceBeacon);
-        // }
 
-        // int overlappingPoints = 0;
-        // int row10overlap = 0;
-        // // Find all possible pairs in the list and find overlap
-        // for (int i = 0; i < _data.Count - 1; i++)
-        // {
-        //     for (int j = i + 1; j < _data.Count; j++)
-        //     {
-        //         // Console.WriteLine($"Considering {_data[i].x},{_data[i].y} and {_data[j].x},{_data[j].y}");
-        //         // First check if they have any overlap
-        //         if (Math.Abs(_data[i].x - _data[j].x) + Math.Abs(_data[i].y - _data[j].y) <= _data[i].distanceBeacon + _data[j].distanceBeacon)
-        //         {
-        //             // Move left to (distancebeacon, distancebeacon), i.e. subtract vector (_data[i].x, _data[i].y) so concentric sphere can be fully in first quadrant
-        //             (int x, int y, int distanceBeacon) newLeft = (_data[i].distanceBeacon >= _data[j].distanceBeacon) ? 
-        //                 (_data[i].distanceBeacon, _data[i].distanceBeacon, _data[i].distanceBeacon) :
-        //                 (_data[j].distanceBeacon, _data[j].distanceBeacon, _data[j].distanceBeacon);
-        //             // Move right to (distancebeacon + dx, distancebeacon + dy)
-        //             (int x, int y, int distanceBeacon) newRight = (_data[i].distanceBeacon >= _data[j].distanceBeacon) ? 
-        //                 (_data[j].x - _data[i].x + _data[i].distanceBeacon, _data[j].y - _data[i].y + _data[i].distanceBeacon, _data[j].distanceBeacon) :
-        //                 (_data[i].x - _data[j].x + _data[j].distanceBeacon, _data[i].y - _data[j].y + _data[j].distanceBeacon, _data[i].distanceBeacon);
-        //
-        //             var totalDistance = 2 * newLeft.distanceBeacon + 1;
-        //             bool[,] bla = new bool[totalDistance, totalDistance];
-        //
-        //             for (var idx = -newLeft.distanceBeacon; idx <= newLeft.distanceBeacon; idx++)
-        //             {
-        //                 for (var idy = -newLeft.distanceBeacon + Math.Abs(idx); idy <= newLeft.distanceBeacon - Math.Abs(idx); idy++)
-        //                 {
-        //                     if (Math.Abs(newRight.x - (newLeft.x + idx)) + Math.Abs(newRight.y - (newLeft.y + idy)) <= newRight.distanceBeacon)
-        //                     {
-        //                         overlappingPoints++;
-        //                         if (_data[i].distanceBeacon >= _data[j].distanceBeacon &&
-        //                             newLeft.x + idx + _data[i].x == 10 ||
-        //                             _data[i].distanceBeacon < _data[j].distanceBeacon &&
-        //                             newLeft.x + idx + _data[j].x == 10)
-        //                         {
-        //                             row10overlap++;
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //             
-        //             // Console.WriteLine($"Found {newLeft.x},{newLeft.y} - D={newLeft.distanceBeacon} and {newRight.x},{newRight.y} - D={newRight.distanceBeacon}");
-        //         }
-        //     }
-        // }
-        
         return _excludedPointsRow.Distinct().Count();
     }
 
@@ -107,6 +46,13 @@ public class Beacons
     {
         _data.Clear();
         _excludedPointsRow.Clear();
+        for (var idx = 0; idx < 4000000; idx++)
+        {
+            _beacons.Add(Enumerable.Repeat(false, 4000000).ToList());
+        }
+        
+        Console.WriteLine("Still here");
+
 
         foreach (var command in File.ReadLines(file))
         {
