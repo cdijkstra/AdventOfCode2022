3bdac136c348a49ee1c90b65371c2955d7af0d60
casper <cdijkstra@xpirit.com>
2022-12-19 20:58:12 +0100

    Story so far

--- Day15/Beacons/Program.cs
+++ Day15/Beacons/Program.cs
@@ -6,7 +6,7 @@ public class Beacons
 {
     private List<(int x, int y, int xBeacon, int yBeacon, int distanceBeacon)> _data = new();
     private List<(int x, int y)> _excludedPointsRow = new();
-    private List<List<bool>> _beacons = new();
+    HashSet<(int x, int y)> _toCheck = new();
     
     private void FillRow(int row)
     {
@@ -34,6 +34,54 @@ public class Beacons
         }
     }
     
+    private void FillGrid()
+    {
+        foreach (var valueTuple in _data)
+        {
+            // Get coordinates next to Manhattan circle
+            var left = (valueTuple.x - valueTuple.distanceBeacon - 1, valueTuple.y);
+            var right = (valueTuple.x + valueTuple.distanceBeacon + 1, valueTuple.y);
+            var down = (valueTuple.x, valueTuple.y - valueTuple.distanceBeacon - 1);
+            var up = (valueTuple.x, valueTuple.y + valueTuple.distanceBeacon + 1);
+
+            List<(int x, int y)> coordinates = new();
+            coordinates
+                .Concat(GetCoordinatesBetween(left, up))
+                .Concat(GetCoordinatesBetween(up, right))
+                .Concat(GetCoordinatesBetween(right, down))
+                .Concat(GetCoordinatesBetween(down, left))
+                .ToList();
+            
+            foreach (var coordinate in coordinates)
+            {
+                
+            }
+        }
+    }
+    
+    private static IEnumerable<(int x, int y)> GetCoordinatesBetween((int x, int y) start, (int x, int y) end)
+    {
+        var (left, right) = start.x <= end.x ? (start, end) : (end, start);
+        var coordinates = new List<(int x, int y)>();
+
+        if (left.y <= end.y)
+        {
+            for (var offset = 0; offset <= right.x - left.x; ++offset)
+            {
+                coordinates.Add((left.x + offset, left.y + offset));
+            }
+        }
+        else
+        {
+            for (var offset = 0; offset <= right.x - left.x; ++offset)
+            {
+                coordinates.Add((left.x + offset, left.y - offset));
+            }
+        }
+
+        return coordinates;
+    }
+    
     public int SolveProblem1(string file, int row)
     {
         Initialize(file);
@@ -41,18 +89,20 @@ public class Beacons
 
         return _excludedPointsRow.Distinct().Count();
     }
+    
+    public int SolveProblem2(string file)
+    {
+        Initialize(file);
+        FillGrid();
+
+        return _toCheck.Count();
+    }
 
     private void Initialize(string file)
     {
         _data.Clear();
         _excludedPointsRow.Clear();
-        for (var idx = 0; idx < 4000000; idx++)
-        {
-            _beacons.Add(Enumerable.Repeat(false, 4000000).ToList());
-        }
-        
-        Console.WriteLine("Still here");
-
+        _data.Clear();
 
         foreach (var command in File.ReadLines(file))
         {
@@ -66,8 +116,6 @@ public class Beacons
             newEntry.yBeacon = yBeacon;
             newEntry.distanceBeacon = Math.Abs(newEntry.x - xBeacon) + Math.Abs(newEntry.y - yBeacon);
             
-            // Console.WriteLine($"Concluded that {xBeacon},{yBeacon} and {newEntry.distanceBeacon}");
-            
             _data.Add(newEntry);
         }
     }
@@ -78,8 +126,9 @@ internal static class Program
     static void Main(string[] args)
     {
         var beacon = new Beacons();
-        beacon.SolveProblem1("dummydata.txt", 10).Should().Be(26);
+        // beacon.SolveProblem1("dummydata.txt", 10).Should().Be(26);
         var answer = beacon.SolveProblem1("data.txt", 2000000);
         Console.WriteLine(answer);
+        beacon.SolveProblem2("data.txt");
     }
 }
