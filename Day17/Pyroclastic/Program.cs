2f8cd8aaf49852060ae4262c6a6caea841a3dcca
casper <cdijkstra@xpirit.com>
2023-01-03 15:07:20 +0100

    New attempt

--- Day17/Pyroclastic/Program.cs
+++ Day17/Pyroclastic/Program.cs
@@ -4,8 +4,8 @@ namespace Space;
 
 public class Pyroclastic
 {
+    private readonly List<List<char>> _rocks = new();
     private List<List<char>> _cave = new();
-    private List<List<char>> _rocks = new();
     private List<char> _jetPattern = new();
     private int _currentIndex = 0;
 
@@ -13,19 +13,19 @@ public class Pyroclastic
     {
         Initialize(file);
 
-        for (var repeats = 0; repeats != 9; repeats++)
+        for (var repeats = 0; repeats != row; repeats++)
         {
             PlaceRock(_rocks[repeats % _rocks.Count()]);
             ApplyJetAndFall();
         }
-        
+
         foreach (var chars in _cave)
         {
             chars.ForEach(x => Console.Write(x));
             Console.WriteLine();
         }
-
-        return _cave.Count();
+        
+        return _cave.Count() - 1;
     }
 
     private void ApplyJetAndFall()
@@ -145,6 +145,8 @@ public class Pyroclastic
 
         var left = leftIndices.Min();
         var right = rightIndices.Max();
+
+        Console.WriteLine($"Moving in {_jetPattern[_currentIndex]}");
         
         if (_jetPattern[_currentIndex] == '<' && left != 1)
         {
@@ -171,98 +173,101 @@ public class Pyroclastic
             }
         }
 
+        Console.WriteLine($"Currentindex = {_currentIndex}");
         _currentIndex = (_currentIndex + 1) % _jetPattern.Count();
+        
+        // foreach (var chars in _cave)
+        // {
+        //     chars.ForEach(x => Console.Write(x));
+        //     Console.WriteLine();
+        // }
     }
 
     private bool RockFalling()
     {
-        int lowestHeightRock = FindLowestRockYPosition();
-        if (lowestHeightRock > FindBottomYPosition() + 1)
-        {
-            _cave.RemoveAt(lowestHeightRock - 1);
-            return true;
-        }
-
         if (FindLowestXPositionsRock().Any(x => _cave[FindLowestRockYPosition() - 1][x] == '#' || _cave[FindLowestRockYPosition() - 1][x] == '-'))
         {
             // In case it is not falling anymore; convert to non-falling rock
             ConvertToNotFallingRock();
+
             return false;
         }
-        else 
-        {
-            // Fall down once more, but now more complex since we cannot remove entry above to fall down
-            var highestRock = FindHighestRockYPosition();
-            var lowestRock = FindLowestRockYPosition();
+        
+        // Fall down once more, but now more complex since we cannot remove entry above to fall down
+        var highestRock = FindHighestRockYPosition();
+        var lowestRock = FindLowestRockYPosition();
+        var floorY = FindBottomYPosition();
 
-            var newEntries = new List<List<char>>();
-            for (var height = lowestRock - 1; height < highestRock; height++)
+        var newEntries = new List<List<char>>();
+        
+        for (var height = lowestRock - 1; height < highestRock; height++)
+        {
+            var newEntry = new List<char>();
+            for (var idx = 0; idx != _cave[0].Count(); idx++)
+            {
+                newEntry.Add(_cave[height][idx] == '#' ? '#' : _cave[height + 1][idx]);
+            }
+            newEntries.Add(newEntry);
+        }
+
+        var entriesToRemove = _cave.Count - lowestRock + 1;
+
+        Console.WriteLine($"FOund {highestRock} and {floorY}");
+        if (highestRock <= floorY)
+        {
+            for (var extraLoop = highestRock; extraLoop <= floorY; extraLoop++)
             {
                 var newEntry = new List<char>();
                 for (var idx = 0; idx != _cave[0].Count(); idx++)
                 {
-                    newEntry.Add(_cave[height][idx] == '#' ? '#' : _cave[height + 1][idx]);
+                    newEntry.Add(_cave[extraLoop][idx] == '@' ? '.' : _cave[extraLoop][idx]);
                 }
                 newEntries.Add(newEntry);
             }
-            
-            foreach (var chars in _cave)
-            {
-                chars.ForEach(x => Console.Write(x));
-                Console.WriteLine();
-            }
-            
-            _cave.RemoveRange(lowestRock - 1, 2 + highestRock - lowestRock);
-            _cave.AddRange(newEntries);
-            
-            Console.WriteLine("And now");
-            foreach (var chars in _cave)
-            {
-                chars.ForEach(x => Console.Write(x));
-                Console.WriteLine();
-            }
-            return true;
         }
+        
+        Console.WriteLine($"ENtries to remove {entriesToRemove}");
+
+        _cave.RemoveRange(lowestRock - 1, _cave.Count - lowestRock + 1);
+        _cave.AddRange(newEntries);
+
+        return true;
     }
 
     private void PlaceRock(List<char> rock)
     {
-        // Find bottom
-        var bottom = FindBottomYPosition();
         // Find height rock
         var heightRock = rock.Count() / 4;
-        var highestHeightInCave = bottom + 4 + heightRock;
-
-        if (_cave.Count() < highestHeightInCave)
+        
+        // Add three empty entries to cave
+        foreach (var repeats in Enumerable.Range(0, 3))
         {
-            // Add new entries to cave
-            var amountOfNewEntries = highestHeightInCave - _cave.Count();
-            foreach (var repeats in Enumerable.Range(0, amountOfNewEntries))
+            List<char> newEntries = new()
             {
-                List<char> newEntries = new()
-                {
-                    '|', '.', '.', '.', '.', '.', '.', '.', '|'
-                };
-                _cave.Add(newEntries);
-            }
+                '|', '.', '.', '.', '.', '.', '.', '.', '|'
+            };
+            _cave.Add(newEntries);
         }
-
+        Console.WriteLine($"Cave count = {_cave.Count}");
+        
+        // Add rock
         for (var rockPart = heightRock - 1; rockPart >= 0; rockPart--)
         {
             List<char> newEntry = new() { '|', '.', '.' };
             newEntry.AddRange(rock.GetRange(rockPart * 4, 4));
             newEntry.AddRange(new List<char>() { '.', '|' });
-            _cave[bottom + 3 + heightRock - rockPart] = newEntry;
+            _cave.Add(newEntry);
         }
     }
 
     private int FindBottomYPosition()
     {
-        for (var idx = _cave.Count() - 1; idx > 0; idx--)
+        for (var idx = _cave.Count() - 1; idx >= 0; idx--)
         {
             List<char> rocks = new List<char>() { '#', '-' };
             if (_cave[idx].GetRange(1, 5).Any(rock => rocks.Contains(rock)))
             {
+                Console.WriteLine($"Found bottom at {idx}");
                 return idx;
             }
         }
@@ -322,14 +327,19 @@ public class Pyroclastic
         _cave.Add(bottom);
     }
 
+
+    
+    
     internal static class Program
     {
         static void Main(string[] args)
         {
             var pyro = new Pyroclastic();
-            pyro.SolveProblem1("dummydata.txt", 10).Should().Be(3068);
-            // beacon.SolveProblem2("dummydata.txt", 20).Should().Be(56000011);
-            // var answer1 = beacon.SolveProblem1("data.txt", 2000000);
+            pyro.SolveProblem1("dummydata.txt", 10).Should().Be(17);
+            // pyro.SolveProblem1("dummydata.txt", 2022).Should().Be(3068);
+            // pyro.SolveProblem2("dummydata.txt", 20).Should().Be(56000011);
+            // var answer1 = pyro.SolveProblem1("data.txt", 2022);
+            // Console.WriteLine(answer1);
             // var answer2 = beacon.SolveProblem2("data.txt", 4000000);
             // Console.WriteLine($"{answer1} and {answer2}");
         }
