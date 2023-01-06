b7fbd422070b8df497af5bc1409170b4a7204878
cdijkstra <cdijkstra@xpirit.com>
2023-01-06 18:10:07 +0100

    Clean solution for Part2

--- Day20/Grove/Program.cs
+++ Day20/Grove/Program.cs
@@ -4,118 +4,113 @@ namespace Grove;
 
 public class Grove
 {
-    public List<int> _positions = new();
-    // Dummy data translates to this;
-    // 0123456
-    // 1023456
-    // 0213456
-    // 0142356
-    // 0135246
-    // 0124635
-    // 0125643
+    private readonly long _decryptionKey = 811589153;
+    private List<long> _positions = new();
+    private List<long> _numbers = new();
     
-    public List<int> _numbers = new();
-    
-    public int SolveProblem1(string file, int row)
+    public long SolveProblem(string file, bool secondExercise = false)
     {
-        Initialize(file);
-        MovePositions();
+        Initialize(file, secondExercise);
+        MovePositions(secondExercise ? 10 : 1);
         
         var posZero = _positions[_numbers.IndexOf(0)];
-        Console.WriteLine($"Found {_numbers[_positions.IndexOf(mod(posZero + 1000, _positions.Count))]}");
-        Console.WriteLine($"Found {_numbers[_positions.IndexOf(mod(posZero + 2000, _positions.Count))]}");
-        Console.WriteLine($"Found {_numbers[_positions.IndexOf(mod(posZero + 3000, _positions.Count))]}");
-
-        
         return _numbers[_positions.IndexOf(mod(posZero + 1000, _positions.Count))] +
                _numbers[_positions.IndexOf(mod(posZero + 2000, _positions.Count))] +
                _numbers[_positions.IndexOf(mod(posZero + 3000, _positions.Count))];
     }
-    
-    private int mod(int num, int modulo) {
+
+    private long mod(long num, long modulo) {
         return (num%modulo + modulo)%modulo;
     }
 
-    private void MovePositions()
+    private void MovePositions(int totalRepetitions = 1)
     {
-        for (var originalIndex = 0; originalIndex != _positions.Count; originalIndex++)
+        for (int repetitions = 0; repetitions != totalRepetitions; repetitions++)
         {
-            var amountOfSteps = _numbers[originalIndex];
-            var currentPos = _positions[originalIndex];
-
-            switch (amountOfSteps)
+            for (var originalIndex = 0; originalIndex != _positions.Count; originalIndex++)
             {
-                case > 0 when currentPos + amountOfSteps < _positions.Count - 1:
+                var amountOfSteps = _numbers[originalIndex];
+                var currentPos = _positions[originalIndex];
+
+                switch (amountOfSteps)
                 {
-                    for (var moveleftIndex = currentPos + 1; moveleftIndex <= currentPos + amountOfSteps; moveleftIndex++)
+                    case > 0 when currentPos + amountOfSteps < _positions.Count - 1:
                     {
-                        _positions[_positions.IndexOf(moveleftIndex)] -= 1;
+                        for (var moveleftIndex = currentPos + 1;
+                             moveleftIndex <= currentPos + amountOfSteps;
+                             moveleftIndex++)
+                        {
+                            _positions[_positions.IndexOf(moveleftIndex)] -= 1;
+                        }
+
+                        _positions[originalIndex] = currentPos + amountOfSteps;
+                        break;
                     }
-                    
-                    _positions[originalIndex] = currentPos + amountOfSteps;
-                    break;
-                }
-                case > 0:
-                {
-                    // Modulo Length - 1 because one extra step is done at right side.
-                    var endPosition = mod(currentPos + amountOfSteps, _positions.Count - 1);
-                    if (endPosition < currentPos)
+                    case > 0:
                     {
-                        for (var moveRightIndex = currentPos - 1; moveRightIndex >= endPosition; moveRightIndex--)
+                        // Modulo Length - 1 because one extra step is done at right side.
+                        var endPosition = mod(currentPos + amountOfSteps, _positions.Count - 1);
+                        if (endPosition < currentPos)
+                        {
+                            for (var moveRightIndex = currentPos - 1; moveRightIndex >= endPosition; moveRightIndex--)
+                            {
+                                _positions[_positions.IndexOf(moveRightIndex)] += 1;
+                            }
+                        }
+                        else if (endPosition > currentPos)
+                        {
+                            for (var moveRightIndex = currentPos + 1; moveRightIndex <= endPosition; moveRightIndex++)
+                            {
+                                _positions[_positions.IndexOf(moveRightIndex)] -= 1;
+                            }
+                        }
+
+                        _positions[originalIndex] = endPosition;
+                        break;
+                    }
+                    case < 0 when currentPos + amountOfSteps > 0:
+                    {
+                        // Note that amountOfSteps is a negative number here! 
+                        for (var moveRightIndex = currentPos - 1;
+                             moveRightIndex >= currentPos + amountOfSteps;
+                             moveRightIndex--)
                         {
                             _positions[_positions.IndexOf(moveRightIndex)] += 1;
                         }
+
+                        _positions[originalIndex] = currentPos + amountOfSteps;
+                        break;
                     }
-                    else if (endPosition > currentPos)
+                    case < 0:
                     {
-                        for (var moveRightIndex = currentPos + 1; moveRightIndex <= endPosition; moveRightIndex++)
+                        var endPosition = currentPos + amountOfSteps == 0
+                            ? _positions.Count - 1
+                            : mod(currentPos + amountOfSteps, _positions.Count - 1);
+
+                        if (endPosition > currentPos)
                         {
-                            _positions[_positions.IndexOf(moveRightIndex)] -= 1;
+                            for (var moveLeftIndex = currentPos + 1; moveLeftIndex <= endPosition; moveLeftIndex++)
+                            {
+                                _positions[_positions.IndexOf(moveLeftIndex)] -= 1;
+                            }
                         }
-                    }
-
-                    _positions[originalIndex] = endPosition;
-                    break;
-                }
-                case < 0 when currentPos + amountOfSteps > 0:
-                {
-                    // Note that amountOfSteps is a negative number here! 
-                    for (var moveRightIndex = currentPos - 1; moveRightIndex >= currentPos + amountOfSteps; moveRightIndex--)
-                    {
-                        _positions[_positions.IndexOf(moveRightIndex)] += 1;
-                    }
-
-                    _positions[originalIndex] = currentPos + amountOfSteps;
-                    break;
-                }
-                case < 0:
-                {
-                    var endPosition = currentPos + amountOfSteps == 0 ? 
-                        _positions.Count - 1 : mod(currentPos + amountOfSteps, _positions.Count - 1);
-
-                    if (endPosition > currentPos)
-                    {
-                        for (var moveLeftIndex = currentPos + 1; moveLeftIndex <= endPosition; moveLeftIndex++)
+                        else if (endPosition < currentPos)
                         {
-                            _positions[_positions.IndexOf(moveLeftIndex)] -= 1;
+                            for (var newRightIndex = currentPos - 1; newRightIndex >= endPosition; newRightIndex--)
+                            {
+                                _positions[_positions.IndexOf(newRightIndex)] += 1;
+                            }
                         }
-                    }
-                    else if (endPosition < currentPos)
-                    {
-                        for (var newRightIndex = currentPos - 1; newRightIndex >= endPosition; newRightIndex--)
-                        {
-                            _positions[_positions.IndexOf(newRightIndex)] += 1;
-                        }
-                    }
 
-                    _positions[originalIndex] = endPosition;
-                    break;
+                        _positions[originalIndex] = endPosition;
+                        break;
+                    }
                 }
             }
         }
     }
 
-    private void Initialize(string file)
+    private void Initialize(string file, bool secondExercise)
     {
         _numbers.Clear();
         _positions.Clear();
@@ -123,7 +118,14 @@ public class Grove
         var idx = 0;
         foreach (var num in File.ReadLines(file))
         {
-            _numbers.Add(int.Parse(num));
+            if (!secondExercise)
+            {
+                _numbers.Add(int.Parse(num));
+            }
+            else
+            {
+                _numbers.Add(int.Parse(num) * _decryptionKey);
+            }
             _positions.Add(idx++);
         }
     }
@@ -135,9 +137,10 @@ internal static class Program
     {
         var grove = new Grove();
 
-        // grove.SolveProblem1("dummydata.txt", 1).Should().Be(3);
-        // grove.SolveProblem1("dummydata2.txt", 1).Should().Be(3);
-        var answer = grove.SolveProblem1("data.txt", 1);
-        Console.WriteLine($"Answer = {answer}");
+        grove.SolveProblem("dummydata.txt").Should().Be(3);
+        grove.SolveProblem("dummydata.txt", true).Should().Be(1623178306);
+        var firstAnswer = grove.SolveProblem("data.txt");
+        var secondAnswer = grove.SolveProblem("data.txt", true);
+        Console.WriteLine($"Answer = {firstAnswer} and {secondAnswer}");
     }
 }
