490650e1cc7fba63d23adc227e4d513a84a59d60
casperdijkstra <casperdijkstra92@gmail.com>
2023-11-12 14:42:49 +0100

    Find shorest path with configurable start point

--- Day12/HillClimbing/Program.cs
+++ Day12/HillClimbing/Program.cs
@@ -10,13 +10,32 @@ public class Hill
 
     private int _nrows;
     private int _ncols;
-    
+
+    private (int x, int y) _destination;
+
+    private readonly List<(int deltaX, int deltaY)> _directions = new()
+    {
+        (0, 1), (1, 0), (0, -1), (-1, 0)
+    };
+
     public int SolveProblem1(string file)
     {
         Initialize(file);
-        var answer = BFS();
-        Console.Write(answer);
-        return answer;
+        var start = FindPosition('S');
+        return BFS(start);
+    }
+
+    public int SolveProblem2(string file)
+    {
+        Initialize(file);
+        List<int> lengths = new();
+        foreach (var start in FindStarts())
+        {
+            var length = BFS(start);
+            lengths.Add(length);
+        }
+
+        return lengths.Where(length => length != -1).Min();
     }
 
     private void Initialize(string file)
@@ -27,7 +46,7 @@ public class Hill
         int maxCols = lines.Max(x => x.Length);
         _heightmap = new char[lines.Length, maxCols];
 
-        for (int i = 0; i < lines.Length; i++)
+        for (var i = 0; i < lines.Length; i++)
         {
             char[] chars = lines[i].ToCharArray();
             for (int j = 0; j < chars.Length; j++)
@@ -39,20 +58,16 @@ public class Hill
         _nrows = _heightmap.GetLength(0);
         _ncols = _heightmap.GetLength(1);
 
-        Console.WriteLine($"Dimensions {_nrows} and {_ncols}");
+        _destination = FindPosition('E');
+        _heightmap[_destination.x, _destination.y] = 'z'; // We know the coordinates, so now set them to a and z so normal rules of heigh difference apply
     }
 
-    private int BFS()
+    private int BFS((int x, int y) start)
     {
-        List<(int deltaX, int deltaY)>
-            directions = new() { (0, 1), (1, 0), (0, -1), (-1, 0) }; // Possible moves: right, down, left, up.
-        var start = FindPosition('S');
-        var destination = FindPosition('E');
         _heightmap[start.x, start.y] = 'a';
-        _heightmap[destination.x, destination.y] = 'z'; // We know the coordinates, so now set them to a and z so normal rules of heigh difference apply
-        
+
         // Implement BFS to find shortest path
-        Queue<(int x, int y, int steps)> queue = new Queue<(int x, int y, int steps)>();
+        Queue<(int x, int y, int steps)> queue = new();
         bool[,] visited = new bool[_nrows, _ncols];
 
         // Enqueue the starting position.
@@ -62,13 +77,13 @@ public class Hill
         while (queue.Count > 0)
         {
             var current = queue.Dequeue();
-            if (current.x == destination.x && current.y == destination.y)
+            if (current.x == _destination.x && current.y == _destination.y)
             {
                 return current.steps;
             }
 
             // go to neighbors if not already visited
-            foreach (var direction in directions)
+            foreach (var direction in _directions)
             {
                 var newX = current.x + direction.deltaX;
                 var newY = current.y + direction.deltaY;
@@ -76,20 +91,18 @@ public class Hill
                 if (!Enumerable.Range(0, _nrows).Contains(newX) ||
                     !Enumerable.Range(0, _ncols).Contains(newY) ||
                     visited[newX, newY]) continue;
-                
+
                 // Check if the elevation change is within limits (same or +1)
                 char currentElevation = _heightmap[current.x, current.y];
                 char newElevation = _heightmap[newX, newY];
                 int elevationDifference = newElevation - currentElevation;
                 if (elevationDifference > 1) continue;
-                
-                Console.WriteLine($"{newX} and {newY}");
-                
+
                 queue.Enqueue((newX, newY, current.steps + 1));
                 visited[newX, newY] = true;
             }
         }
-        
+
         // If no path is found.
         return -1;
     }
@@ -109,16 +122,33 @@ public class Hill
 
         throw new Exception("Not found");
     }
-}
 
-internal static class Program
-{
-    static async Task Main(string[] args)
+    private List<(int x, int y)> FindStarts()
     {
-        var hill = new Hill();
-        // hill.SolveProblem1("dummydata.txt").Should().Be(31);
-        hill.SolveProblem1("data.txt");
-        // var solution1 = hill.SolveProblem1("data.txt");
-        // solution1.Should().Be(12);
+        List<(int x, int y)> starts = new();
+        for (int i = 0; i < _nrows; i++)
+        {
+            for (int j = 0; j < _ncols; j++)
+            {
+                if (_heightmap[i, j] is 'a' or 'S')
+                {
+                    starts.Add((i, j));
+                }
+            }
+        }
+
+        return starts;
+    }
+
+    internal static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hill = new Hill();
+            hill.SolveProblem1("dummydata.txt").Should().Be(31);
+            Console.WriteLine(hill.SolveProblem1("data.txt"));
+            hill.SolveProblem2("dummydata.txt").Should().Be(29);
+            Console.WriteLine(hill.SolveProblem2("data.txt"));
+        }
     }
 }
